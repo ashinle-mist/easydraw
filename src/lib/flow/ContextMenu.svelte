1da5ec24b1fed71bbf4b8079214daf829cc9137e
ashinle-mist <raiyanap19@gmail.com>
2026-01-10 18:32:24 +1100

    add context menu

---
+++ src/lib/flow/ContextMenu.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+	import { useEdges, useNodes, useSvelteFlow } from '@xyflow/svelte';
+	import { nanoid } from 'nanoid';
+
+	let {
+		id,
+		top,
+		left,
+		right,
+		bottom,
+		onclick,
+	}: {
+		id: string;
+		top: number | undefined;
+		left: number | undefined;
+		right: number | undefined;
+		bottom: number | undefined;
+		onclick: () => void;
+	} = $props();
+
+	const { deleteElements } = useSvelteFlow();
+
+	const nodes = useNodes();
+
+	function duplicateNode() {
+		const node = nodes.current.find((node) => node.id === id);
+		if (node) {
+			nodes.current = [
+				...nodes.current,
+				{
+					...node,
+					id: nanoid(),
+					position: {
+						x: node.position.x,
+						y: node.position.y + 50,
+					},
+				},
+			];
+		}
+	}
+
+	function deleteNode() {
+		deleteElements({ nodes: [{ id }] });	}
+</script>
+
+
+<div
+	style="top: {top}px; left: {left}px; right: {right}px; bottom: {bottom}px;"
+	class="context-menu"
+	{onclick}
+	onpointerdown={(e) => e.stopPropagation()}
+>
+	<p style="margin: 0.5em;">
+		<small>node: {id}</small>
+	</p>
+	<button onclick={duplicateNode}>duplicate</button>
+	<button onclick={deleteNode}>delete</button>
+</div>
+
+<style>
+    .context-menu {
+        background: white;
+        border-style: solid;
+        box-shadow: 10px 19px 20px rgba(0, 0, 0, 10%);
+        position: absolute;
+        z-index: 10;
+    }
+
+    .context-menu button {
+        border: none;
+        display: block;
+        padding: 0.5em;
+        text-align: left;
+        width: 100%;
+    }
+
+    .context-menu button:hover {
+        background: white;
+    }
+</style>
--- src/lib/flow/Flow.svelte
+++ src/lib/flow/Flow.svelte
@@ -7,10 +7,13 @@
 		MiniMap,
 		useSvelteFlow,
 		type Node,
+		type Edge,
+		type NodeEventWithPointer
 	} from '@xyflow/svelte';
 
 	import { useDnD } from '$lib/flow/DnDProvider.svelte';
 	import Sidebar from '$lib/components/Sidebar.svelte';
+	import ContextMenu from '$lib/flow/ContextMenu.svelte';
 
 	import '@xyflow/svelte/dist/style.css';
 
@@ -19,8 +22,8 @@
 			id: '1',
 			type: 'default',
 			data: { label: 'Drag nodes to the canvas' },
-			position: { x: 0, y: 0 },
-		},
+			position: { x: 0, y: 0 }
+		}
 	]);
 
 	let edges = $state.raw([]);
@@ -50,36 +53,86 @@
 
 		const position = screenToFlowPosition({
 			x: event.clientX,
-			y: event.clientY,
+			y: event.clientY
 		});
 
 		const newNode = {
 			id: `${Math.random()}`,
 			type: type.current,
 			position,
-			data: { label: `${type.current} node`},
-			origin: [0.5, 0.0],
+			data: { label: `${type.current} node` },
+			origin: [0.5, 0.0]
 		} satisfies Node;
 
 		nodes = [...nodes, newNode];
 	};
+
+	let menu: {
+		id: string;
+		top?: number;
+		left?: number;
+		right?: number;
+		bottom?: number;
+	} | null = $state(null);
+	let clientWidth: number = $state(0);
+	let clientHeight: number = $state(0);
+
+	const handleContextMenu: NodeEventWithPointer<MouseEvent> = ({ event, node }) => {
+		// Prevent native context menu from showing
+		event.preventDefault();
+
+		// Calculate position of the context menu. We want to make sure it
+		// doesn't get positioned off-screen.
+		menu = {
+			id: node.id,
+			top: event.clientY < clientHeight - 200 ? event.clientY : undefined,
+			left: event.clientX < clientWidth - 200 ? event.clientX : undefined,
+			right: event.clientX >= clientWidth - 200 ? clientWidth - event.clientX : undefined,
+			bottom: event.clientY >= clientHeight - 200 ? clientHeight - event.clientY : undefined
+		};
+	};
+
+	// Close the context menu if it's open whenever the window is clicked.
+	function handlePaneClick() {
+		menu = null;
+	}
 </script>
 
-<main style:width="100vw" style:height="100vh">
-	<SvelteFlow bind:nodes bind:edges {defaultEdgeOptions} fitView ondragover={onDragOver} ondrop={onDrop}>
+<main style="width:100vw; height:100vh;" bind:clientWidth bind:clientHeight>
+	<SvelteFlow
+			bind:nodes
+			bind:edges
+			{defaultEdgeOptions}
+			fitView
+			ondragover={onDragOver}
+			ondrop={onDrop}
+			onnodecontextmenu={handleContextMenu}
+			onpaneclick={handlePaneClick}
+			onpointerdown={handlePaneClick}
+	>
 		<Background variant={BackgroundVariant.Dots} />
+		{#if menu}
+			<ContextMenu
+				onclick={handlePaneClick}
+				id={menu.id}
+				top={menu.top}
+				left={menu.left}
+				right={menu.right}
+				bottom={menu.bottom}
+			/>
+		{/if}
 		<MiniMap />
 
 	</SvelteFlow>
 	<Sidebar />
-	<Controls position="top-right"/>
+	<Controls position="top-right" />
 
 </main>
 
 <style>
-	main {
-			height: 100vh;
-			display: flex;
-			flex-direction: column-reverse;
-	}
+    main {
+        height: 100vh;
+        display: flex;
+        flex-direction: column-reverse;
+    }
 </style>
